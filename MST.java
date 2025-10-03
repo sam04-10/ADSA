d0f0250fff601cef791287125c48a55f1855e844
sam04-10 <samruddhi.2403354209@vcet.edu.in>
2025-10-03 08:40:52 +0530

    Update MST.java

--- MST.java
+++ MST.java
@@ -1,66 +1,100 @@
-import java.util.*;
-public class ops
+import java.util.Scanner;
+public class PrimMST
 {
-// Swap two elements in the array
-public static void swap(int[] arr, int i, int j)
+ private int findMinKey(int[] key, boolean[] mstSet, int V)
 {
-int temp = arr[i];
-arr[i] = arr[j];
-arr[j] = temp;
-}
-// Optimized bubble sort implementation
-public static void bubbleSort(int[] arr)
+ int min = Integer.MAX_VALUE;
+int min_index = -1;
+ for (int v = 0; v < V; v++)
 {
-int n = arr.length;
-boolean swapped;
-for (int i = 0; i < n - 1; i++)
-{
-swapped = false;
-for (int j = 0; j < n - 1 - i; j++)
-{
-if (arr[j] > arr[j + 1]){
+ if (!mstSet[v] && key[v] < min)
+{ min = key[v];
+min_index = v;
 }
 }
+ return min_index;
+}
+ private void printMST(int[] parent, int[][] graph, int V)
+{ int totalCost =
+0;
+System.out.println("\nMinimum Spanning Tree:");
+System.out.println("Edge \t Weight");
+ for (int i = 1; i < V; i++)
 {
+ int edgeWeight = graph[i][parent[i]];
+ System.out.println(parent[i] + " - " + i + "\t " + edgeWeight);
+totalCost += edgeWeight;
 }
+ System.out.println("\nTotal cost of MST: " + totalCost);
 }
-}
+ public void primMST(int[][] graph, int V)
 {
+ int[] parent = new int[V]; int[]
+key = new int[V]; boolean[] mstSet
+= new boolean[V];
+ for (int i = 0; i < V; i++)
 {
-swap(arr, j, j + 1);
-swapped = true;
-// If no two elements were swapped, array is already sorted
-if (!swapped)
-break;
-public static void main(String[] args)
-Scanner sc = new Scanner(System.in);
-System.out.print("Enter the number of files: ");
-int num = sc.nextInt();
-int[] sizes = new int[num];
-for (int i = 0; i < num; i++)
-System.out.print("Enter the size of File " + (i + 1) + ": ");sizes[i] = sc.nextInt();
+ key[i] = Integer.MAX_VALUE;
+mstSet[i] = false;
+
 }
-// Step 1: Sort file sizes
-bubbleSort(sizes);
-// Step 2: Calculate cumulative retrieval times
-int[] retrievalTimes = new int[num];
-retrievalTimes[0] = sizes[0];
-for (int i = 1; i < num; i++)
+ key[0] = 0; parent[0]
+= -1;
+ for (int count = 0; count < V - 1; count++)
 {
-retrievalTimes[i] = retrievalTimes[i - 1] + sizes[i];
-}
-// Step 3: Compute Mean Retrieval Time
-float totalRetrievalTime = 0;
-for (int time : retrievalTimes)
+ int u = findMinKey(key, mstSet, V);
+mstSet[u] = true; for (int v = 0; v <
+V; v++)
 {
-totalRetrievalTime += time;
+ if (graph[u][v] != 0 && !mstSet[v] && graph[u][v] < key[v])
+{ parent[v] = u;
+key[v] = graph[u][v];
+}
+
+}
+
+}
+ printMST(parent, graph, V);
+
+}
+ public static void main(String[] args)
 {
-float meanRetrievalTime = totalRetrievalTime / num;
-// Step 4: Display results
-System.out.println("\nSorted File Sizes: " + Arrays.toString(sizes));}
+ Scanner sc = new Scanner(System.in);
+System.out.print("Enter the number of vertices: ");
+ int V = sc.nextInt();
+ int[][] graph = new int[V][V];
+ System.out.println("Enter the adjacency matrix for the graph (enter 0 for no edge):");
+ for (int i = 0; i < V; i++)
+{ for (int j = 0; j < V;
+j++)
+{ graph[i][j] =
+sc.nextInt();
+ if (i == j)
+{ graph[i][j] =
+0;
+
 }
-System.out.println("Retrieval Times: " + Arrays.toString(retrievalTimes));
-System.out.printf("The Mean Retrieval Time is %.2f%n", meanRetrievalTime);
-sc.close();
+
 }
+
 }
+ PrimMST mst = new PrimMST();
+mst.primMST(graph, V);
+ sc.close();
+}
+ }
+Ouput ;
+Enter the number of vertices: **5**
+Enter the adjacency matrix for the graph (enter 0 for no edge):
+0 2 0 6 0
+2 0 3 8 5
+0 3 0 0 7
+6 8 0 0 9
+0 5 7 9 0
+Minimum Spanning Tree:
+Edge Weight
+0 - 1 2
+1 - 2 3
+0 - 3 6
+1 - 4 5
+Total cost of MST: 16
